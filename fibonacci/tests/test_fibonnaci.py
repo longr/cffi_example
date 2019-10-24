108b55a2f9e337f614ec6332888f4e66af8a3dab
Robin Long <robin.long1@hotmail.co.uk>
2019-10-24 15:17:25 +0100

    Removed intentionally broken code and old debug code.

--- fibonacci/tests/test_fibonnaci.py
+++ fibonacci/tests/test_fibonnaci.py
@@ -1,73 +1,49 @@
 import pytest
 import fibonacci
 
-print("Check fibonacci.fib")
-
-
 def test_fib_check_zeroth():
-    print("1")
     assert fibonacci.fib(0) == 0
 
-
+   
 def test_fib_check_first():
-    print("2")
     assert fibonacci.fib(1) == 1
 
-
+   
 def test_fib_check_tenth():
-    print("3")
-    assert fibonacci.fib(10) == 52 #break test with wrong answer
-    #assert fibonacci.fib(10) == 55
-
-
-print("Check fibonacci.cfib")
+    assert fibonacci.fib(10) == 55
 
 
 def test_cfib_check_zeroth():
-    print("4")
     assert fibonacci.cfib(0) == 0
 
 
 def test_cfib_check_first():
-    print("5")
     assert fibonacci.cfib(1) == 1
 
 
 def test_cfib_check_tenth():
-    print("6")
     assert fibonacci.cfib(10) == 55
 
 
-print("Check fibonacci.fast_fib")
-
 def test_fast_fib_check_zeroth():
-    print('7')
     assert fibonacci.fast_fib(0) == 0
 
 
 def test_fast_fib_check_first():
-    print("8")
     assert fibonacci.fast_fib(1) == 1
 
 
 def test_fast_fib_check_tenth():
-    print("9")
     assert fibonacci.fast_fib(10) == 55
 
-
-print("Check fibonacci.cfast_fib")
-
-
+    
 def test_cfast_fib_check_zeroth():
-    print("10")
     assert fibonacci.cfast_fib(0) == 0
 
 
 def test_cfast_fib_check_first():
-    print("11")
     assert fibonacci.cfast_fib(1) == 1
 
 
 def test_cfast_fib_check_tenth():
-    print("12")
     assert fibonacci.cfast_fib(10) == 55
