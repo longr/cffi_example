888f1fdf846d72667c7dab1f463437bec7be2256
Robin Long <robin.long1@hotmail.co.uk>
2019-11-12 15:28:43 +0000

    Added tests t achieve 100% coverage

--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -3,3 +3,9 @@ import fibonacci
 
 def test_fib_check_zero():
     assert fibonacci.fib(0) == 0
+
+def test_fib_check_one():
+    assert fibonacci.fib(1) == 1
+
+def test_fib_check_ten():
+    assert fibonacci.fib(10) == 55
